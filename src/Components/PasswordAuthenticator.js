53601fc8ffbe78deda157535b88db060fbae5cab
namusanga <namusanganicho@gmail.com>
2021-07-25 10:03:45 -0700

    added password verification

--- src/Components/PasswordAuthenticator.js
+++ src/Components/PasswordAuthenticator.js
@@ -2,9 +2,38 @@
 import TextField from "@material-ui/core/TextField";
 import {Button} from "@material-ui/core";
 import "./PasswordAuthenticator.css";
+import firebase from "firebase";
 
 
 class PasswordAuthenticator extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            passwordField: "nassing"
+        }
+        
+    }
+    
+    accessPassword = () =>{
+        const db = firebase.firestore()
+        const passwords = db.collection("Passwords").doc("VsUSw5pdyhAe1uJKrN4M").get().then((doc)=>{
+            var item = doc.data().All_passwords;
+            if(doc.data().All_passwords
+                .includes(this.state.passwordField)){
+                //CORRECT PASSWORD CODE HERE
+                console.log("perfect")
+                
+            }else{
+                //WRONG PASSWORD CODE HERE
+                console.log("there's nothing here")
+                
+            }
+        })
+    }
+
+
     render() {
         return (
             <div>
@@ -14,8 +43,17 @@ class PasswordAuthenticator extends Component {
                         label="Password"
                         type="password"
                         autoComplete="current-password"
+                        onChange = {(event)=>{
+                            this.setState({passwordField: event.target.value})
+                        }}
                     />
-                    <Button variant="contained" color="primary">
+                    
+                    <Button variant="contained" color="primary"  onClick={()=>{
+                       
+                        
+                        this.accessPassword()
+                        
+                    }}>
                         Log In
                     </Button>
                 </form>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase";
+
+firebase.initializeApp({
+    apiKey: "AIzaSyDH1SUzizX2w-Dxt3K1EYV8k2J-oIEzMoo",
+    authDomain: "lucky2-4fbcf.firebaseapp.com",
+    projectId: "lucky2-4fbcf",
+    storageBucket: "lucky2-4fbcf.appspot.com",
+    messagingSenderId: "158697506044",
+    appId: "1:158697506044:web:c639950255836a12a18379",
+    measurementId: "G-DG4L1ZLWB5"
+})
 
 ReactDOM.render(
   <React.StrictMode>
