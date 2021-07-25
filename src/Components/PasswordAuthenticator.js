144310f35479bb1630930b076afc81483734eeff
namusanga <namusanganicho@gmail.com>
2021-07-25 03:06:39 -0700

    additional fioles

--- src/Components/PasswordAuthenticator.js
+++ src/Components/PasswordAuthenticator.js
@@ -1,10 +1,13 @@
 ﻿import React, {Component} from 'react';
+import {TextField} from ''
 
 class PasswordAuthenticator extends Component {
     render() {
         return (
             <div>
-                
+                <form className={classes.root} noValidate autoComplete="off">
+                    <TextField id = "standard-basic" label={"Standard"}/>
+                </form>
             </div>
         );
     }
